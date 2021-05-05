179a142436ac4d0a4a2f49e6bfbfcf96cd6a7e8d
Adam Stradomski <>
2021-05-05 20:30:36 +0000

    Fixed a bug with Bye added check for tournament id

--- commands/CobraCommand.py
+++ commands/CobraCommand.py
@@ -6,8 +6,7 @@ from discord import Embed, Colour
 COBRA_URL = "http://cobr.ai/tournaments/"
 
 
-class CobraCommand(commands.Cog,
-                   name="Cobr.AI Integration"):
+class CobraCommand(commands.Cog, name="Cobr.AI Integration"):
     """
     Integrate your Discord channel with cobr.ai Netrunner Tournament using Discord bot !cobra command
     """
@@ -20,159 +19,182 @@ class CobraCommand(commands.Cog,
     guilds = {}
 
     def getTournamentId(self, ctx):
+        """ Get current tournament ID provided by SET command.  """
         return self.guilds[ctx.guild.id][ctx.channel.id]["tournamentID"]
 
-    def requireTournamentId(func):
-        """
-        Decorator that verifies if tournament ID was set on the channel and notifies users if not. 
-        Works with bot.command methods. Requires attributes (CobraCommand, ctx) 
-        """
-        async def wrapper(*args, **kwargs):
-            self = args[0]
-            ctx = args[1]
+    def getTournament(self, ctx):
+        """ Connect to Cobra.AI and get tournament current information. """
+        try:
+            return requests.get(COBRA_URL + self.getTournamentId(ctx) +
+                                ".json").json()
+        except Exception:
+            return None
 
-            if self.guilds.get(ctx.guild.id) and self.guilds.get(
-                    ctx.guild.id).get(ctx.channel.id) and self.guilds.get(
-                        ctx.guild.id).get(ctx.channel.id).get("tournamentID"):
-                await func(*args, **kwargs)
-            else:
-                await ctx.send(
-                    "Before using other commands, please set Tournament ID using !cobra set"
-                )
+    async def cog_check(self, ctx):
+        """ Verify if the tournametn ID was set, before using any of commands. Expect set command."""
+        if ctx.invoked_with == "set" or ctx.invoked_with == "cobra" or ctx.invoked_with == "help":
+            return True  # !set, !cobra - can be invoked always
 
-        return wrapper
+        # Require tournamentID for all other commands
+        if self.guilds.get(ctx.guild.id) and self.guilds.get(ctx.guild.id).get(
+                ctx.channel.id) and self.guilds.get(ctx.guild.id).get(
+                    ctx.channel.id).get("tournamentID"):
+            return True
+        else:
+            await ctx.send(
+                "!! Before using other commands, please set Tournament ID using !cobra set"
+            )
+            return False
 
-    @commands.group(pass_context=True)
+    @commands.group(pass_context=True, invoke_without_command=True)
     async def cobra(self, ctx):
-        """
-        set|show|standings|pairings|stats
-        """
-        pass
+        """ set {ID} | set http://cobr.ai/tournaments/{ID} | show | standings | pairings | pairings {ROUND} | stats - !help cobra for more info"""
+        await ctx.send("!help cobra for more info")
 
     @cobra.command(name="set")
     async def set(self, ctx, id: str):
-        """Set the tournament ID by either copy-past tournament URL (eg. http://cobr.ai/tournaments/2029) or the tournament ID for cobr.ai (eg. 2029) """
+        """Sets the tournament id
+         by either copy-past tournament URL (eg. http://cobr.ai/tournaments/2029) or the tournament ID for cobr.ai (eg. 2029) """
 
         # extract tournament id from URL: http://cobr.ai/tournaments/2029 -> 2029
         id = id.strip().split("/")[-1]
         if id.isnumeric():
-            self.guilds[ctx.guild.id] = {}
+            if not self.guilds.get(ctx.guild.id):
+                self.guilds[ctx.guild.id] = {}
             self.guilds[ctx.guild.id][ctx.channel.id] = {}
             self.guilds[ctx.guild.id][ctx.channel.id]["tournamentID"] = id
-            self.guilds[ctx.guild.id][ctx.channel.id]["players"] = {}
-            await ctx.send("Tournament ID set to {}".format(id))
-        else:
-            await ctx.send("""Wrong tournament id format. Example use:
-!cobra set 2029
-!cobra set http://cobr.ai/tournaments/2029
-          """)
 
-    @cobra.command(name="show", description="Show basic tournament information")
-    @requireTournamentId
+            data = self.getTournament(ctx)
+            if data:
+                await ctx.send("Set current tournament to '{}'".format(
+                    data.get("name")))
+            else:
+                self.guilds[ctx.guild.id][
+                    ctx.channel.id]["tournamentID"] = None
+                await ctx.send("Tournament Not found: {}{}".format(
+                    COBRA_URL, id))
+        else:
+            await ctx.send(
+                """Wrong tournament id format. Example use:\n!cobra set 2029\n!cobra set http://cobr.ai/tournaments/2029"""
+            )
+
+    @cobra.command(name="show",
+                   description="Show basic tournament information")
     async def show(self, ctx):
-        """Show the tournament infomation"""
-        data = requests.get(COBRA_URL + self.getTournamentId(ctx) +".json").json()
+        """Show basic tournament infomation"""
+        data = self.getTournament(ctx)
+
         embed = Embed()
         embed.title = data.get("name")
         embed.url = COBRA_URL + self.getTournamentId(ctx)
-        embed.color=Colour.dark_green()
-        embed.add_field(name="Organizer", value=data.get("tournamentOrganiser").get("nrdbUsername"), inline=True)
-        embed.add_field(name="Players", value=len(data.get("players")), inline=True)
-        embed.add_field(name="Rounds", value=data.get("preliminaryRounds"), inline=True)
+        embed.color = Colour.dark_green()
+        embed.add_field(
+            name="Organizer",
+            value=data.get("tournamentOrganiser").get("nrdbUsername"),
+            inline=True)
+        embed.add_field(name="Players",
+                        value=len(data.get("players")),
+                        inline=True)
+        embed.add_field(name="Rounds",
+                        value=data.get("preliminaryRounds"),
+                        inline=True)
 
         await ctx.send(embed=embed)
 
     @cobra.command(name="standings")
-    @requireTournamentId
     async def standings(self, ctx):
-        """Show the current tournament standings. """
-        data = requests.get(COBRA_URL + self.getTournamentId(ctx) +".json").json()
-        ranks,names,mps="","",""
+        """Show current tournament standings. """
+        data = self.getTournament(ctx)
+        ranks, names, mps = "", "", ""
 
-        for player in data.get("players"): 
-          self.logger.log(logging.DEBUG, player)
+        for player in data.get("players"):
+            self.logger.log(logging.DEBUG, player)
 
-          ranks = ranks + str(player["rank"]) + "\n"
-          names = names + str(player["name"]) + "\n"
-          mps = mps + str(player["matchPoints"]) + "\n"
+            ranks = ranks + str(player["rank"]) + "\n"
+            names = names + str(player["name"]) + "\n"
+            mps = mps + str(player["matchPoints"]) + "\n"
 
         embed = Embed()
         embed.title = data.get("name")
         embed.description = "Standings round " + str(len(data.get("rounds")))
         embed.url = COBRA_URL + self.getTournamentId(ctx)
-        embed.color=Colour.red()
+        embed.color = Colour.red()
         embed.add_field(name="Rank", value=ranks, inline=True)
         embed.add_field(name="Name", value=names, inline=True)
-        embed.add_field(name="Points", value=mps, inline=True)  
+        embed.add_field(name="Points", value=mps, inline=True)
         await ctx.send(embed=embed)
 
     @cobra.command(name="pairings")
-    @requireTournamentId
-    async def pairings(self, ctx):
-        """Show the current tournament pairing. """
-        data = requests.get(COBRA_URL + self.getTournamentId(ctx) +".json").json()
+    async def pairingsRound(self, ctx, round: int = -1):
+        """Show current tournament pairings. Latest round is default. """
+        data = self.getTournament(ctx)
 
-        # Create Map PlayerID:PlayerName        
+        # Create Dictionary PlayerID:PlayerName for pairings
+        players = {None: "Bye"}  # Null players are Bye
         for player in data.get("players"):
-            self.guilds[ctx.guild.id][ctx.channel.id]["players"].update({player.get("id"): player.get("name")}) 
-        
-        self.logger.log(logging.DEBUG, self.guilds[ctx.guild.id][ctx.channel.id]["players"])
+            players.update({player.get("id"): player.get("name")})
 
-        tables,player1,player2="","",""
+        if round < 0: 
+          round = len(data.get("rounds")) - 1
+        else: 
+          round -= 1
 
-        # Loop over last round
-        for pair in data.get("rounds")[-1]: 
-          self.logger.log(logging.DEBUG, pair)
-          p1 = pair.get("player1").get("id")
-          p2 = pair.get("player2").get("id")
-          tables = tables + str(pair.get("table")) + "\n"
-          player1 = player1 + self.guilds[ctx.guild.id][ctx.channel.id]["players"][p1] + "\n"
-          player2 = player2 + self.guilds[ctx.guild.id][ctx.channel.id]["players"][p2] + "\n"
+        tables, player1, player2 = "", "", ""
+
+        # Loop over each pair in round
+        for pair in data.get("rounds")[round]:
+            self.logger.log(logging.DEBUG, pair)
+
+            p1 = pair.get("player1").get("id")
+            p2 = pair.get("player2").get("id")
+
+            tables = tables + str(pair.get("table")) + "\n"
+            player1 += players[p1] + "\n"
+            player2 += players[p2] + "\n"
 
         embed = Embed()
         embed.title = data.get("name")
-        embed.description = "Pairings round " + str(len(data.get("rounds")))
+        embed.description = "Pairings for round " + str(round + 1)
         embed.url = COBRA_URL + self.getTournamentId(ctx)
-        embed.color=Colour.dark_orange()
+        embed.color = Colour.dark_orange()
         embed.add_field(name="Table", value=tables, inline=True)
         embed.add_field(name="Player 1", value=player1, inline=True)
-        embed.add_field(name="Player 2", value=player2, inline=True)  
+        embed.add_field(name="Player 2", value=player2, inline=True)
         await ctx.send(embed=embed)
 
     @cobra.command(name="stats")
-    @requireTournamentId
     async def stats(self, ctx):
         """Show the current tournament Corp and Runner ID statistics. """
-        data = requests.get(COBRA_URL + self.getTournamentId(ctx) +".json").json()
+        data = self.getTournament(ctx)
 
         cids = {}
         rids = {}
-        # Create Map PlayerID:PlayerName        
+        # Create Map PlayerID:PlayerName
         for player in data.get("players"):
-          cid = cids.get(player.get("corpIdentity"))
-          if not cid: 
-            cid = 0
-          cids.update({player.get("corpIdentity"): cid + 1})
-          
-          rid = rids.get(player.get("runnerIdentity"))
-          if not rid: 
-            rid = 0          
-          rids.update({player.get("runnerIdentity"): rid + 1})
+            cid = cids.get(player.get("corpIdentity"))
+            if not cid:
+                cid = 0
+            cids.update({player.get("corpIdentity"): cid + 1})
 
-        corp,runner,ccount,rcount="","","",""
+            rid = rids.get(player.get("runnerIdentity"))
+            if not rid:
+                rid = 0
+            rids.update({player.get("runnerIdentity"): rid + 1})
+
+        corp, runner, ccount, rcount = "", "", "", ""
         for id in cids:
-          corp = corp + id + "\n"
-          ccount = ccount + str(cids[id]) + "\n"
+            corp = corp + id + "\n"
+            ccount = ccount + str(cids[id]) + "\n"
 
         for id in rids:
-          runner = runner + id + "\n"
-          rcount = rcount + str(rids[id]) + "\n"
+            runner = runner + id + "\n"
+            rcount = rcount + str(rids[id]) + "\n"
 
         embed = Embed()
         embed.title = data.get("name")
         embed.description = "Statistics"
         embed.url = COBRA_URL + self.getTournamentId(ctx)
-        embed.color=Colour.dark_magenta()
+        embed.color = Colour.dark_magenta()
         embed.add_field(name="Corp IDs", value=corp, inline=True)
         embed.add_field(name="No", value=ccount, inline=True)
         embed.add_field(name="\u200B", value="\u200B", inline=False)
